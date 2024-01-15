3331287ca917aaf143063fd1bfdee555b387b4c2
Nafer0208 <115758460+Nafer0208@users.noreply.github.com>
2024-01-15 15:08:02 -0600

    Add files via upload

--- funt.js
+++ funt.js
@@ -1,5 +1,5 @@
 function funtno(){
-    document.getElementById("btnNO").innerHTML="Ni lo intentes 😡";
+    document.getElementById("btnNO").innerHTML="¿Cómo así? 😡";
     document.getElementById("main").style.backgroundColor = "#F25C5C";
     document.getElementById("textoMain").style.color = "#FFFFFF";
 }
